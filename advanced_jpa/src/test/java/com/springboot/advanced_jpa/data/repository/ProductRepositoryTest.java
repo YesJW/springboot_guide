96133707b22f3363fa7c0f9b8fb20fe088bee0cb
Jangwoo No <ssk01017@gmail.com>
2023-01-14 01:42:52 +0900

    advanced_jpa practice

---
+++ advanced_jpa/src/test/java/com/springboot/advanced_jpa/data/repository/ProductRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.springboot.advanced_jpa.data.repository;
+
+import com.springboot.advanced_jpa.data.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+public class ProductRepositoryTest {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    void sortingAndPagingTest(){
+        Product product1 = new Product();
+        product1.setName("펜");
+        product1.setPrice(1000);
+        product1.setStock(100);
+        product1.setCreateAt(LocalDateTime.now());
+        product1.setUpdateAt(LocalDateTime.now());
+
+        Product product2 = new Product();
+        product2.setName("펜");
+        product2.setPrice(5000);
+        product2.setStock(300);
+        product2.setCreateAt(LocalDateTime.now());
+        product2.setUpdateAt(LocalDateTime.now());
+
+        Product product3 = new Product();
+        product3.setName("펜");
+        product3.setPrice(500);
+        product3.setStock(50);
+        product3.setCreateAt(LocalDateTime.now());
+        product3.setUpdateAt(LocalDateTime.now());
+
+        Product savedProduct1 = productRepository.save(product1);
+        Product savedProduct2 = productRepository.save(product2);
+        Product savedProduct3 = productRepository.save(product3);
+
+        System.out.println(productRepository.findByName("펜", Sort.by(Order.asc("price"))));
+        System.out.println(productRepository.findByName("펜", Sort.by(Order.asc("price"), Order.desc("stock"))));
+        System.out.println(productRepository.findByName("펜",getSort()));
+
+        Page<Product> productPage = productRepository.findByName("펜", PageRequest.of(0, 2));
+        System.out.println(productPage.getContent()); //Page 객체의 데이터 출력
+
+    }
+
+    private Sort getSort(){
+        return Sort.by(
+                Order.asc("price"),
+                Order.desc("stock")
+        );
+    }
+
+}
